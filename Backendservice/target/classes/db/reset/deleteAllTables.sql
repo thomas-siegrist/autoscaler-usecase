04ee58026a1ca55200f95fa1bee870402a908e46
u217269 <thomas.siegrist@sbb.ch>
2016-07-29 16:06:48 +0200

    Removing and Git-Ignoring target folder

--- Backendservice/target/classes/db/reset/deleteAllTables.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS shopping_cart_items;
-DROP TABLE IF EXISTS shopping_cart_item;
-DROP TABLE IF EXISTS shopping_cart;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS article;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS schema_version;
