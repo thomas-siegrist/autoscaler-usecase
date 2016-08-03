5657b1372ea64ff478da504a131acfed90a5f34c
Thomas Siegrist <thomas@carmens-macbook-pro.home>
2016-08-03 21:47:40 +0200

    New Service: Emailservice

---
+++ Emailservice/src/main/java/ch/sbb/cloud/autoscaler/usecase/EmailserviceApplication.java
@@ -0,0 +1,12 @@
+package ch.sbb.cloud.autoscaler.usecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailserviceApplication.class, args);
+	}
+}
